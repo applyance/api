5557a4be95c40060d4584e3236cf23208cd98085
Stephen Watkins <stjowa@gmail.com>
2014-08-08 21:38:02 -0500

    Fixing blueprint sync for entities and spots.

--- models/blueprint.rb
+++ models/blueprint.rb
@@ -6,17 +6,15 @@ module Applyance
 
     # Go through child entities and spots, deleting the same blueprints if they exist
     def ensure_unique_in_chain
-      # if self.entity
-      #   self.entity.entities.each do |entity|
-      #     entity.blueprints_dataset.where(:definition_id => self.definition_id).delete
-      #     entity.spots.each do |spot|
-      #       spot.blueprints_dataset.where(:definition_id => self.definition_id).delete
-      #     end
-      #   end
-      #   self.entity.spots.each do |spot|
-      #     spot.blueprints_dataset.where(:definition_id => self.definition_id).delete
-      #   end
-      # end
+      self.entity.entities.each do |entity|
+        entity.blueprints_dataset.first(:definition_id => self.definition_id).destroy
+        entity.spots.each do |spot|
+          spot.blueprints_dataset.first(:definition_id => self.definition_id).destroy
+        end
+      end
+      self.entity.spots.each do |spot|
+        spot.blueprints_dataset.first(:definition_id => self.definition_id).destroy
+      end
     end
 
   end
