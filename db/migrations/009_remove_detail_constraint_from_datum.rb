0a0041945f1b0e116836689785c1ec8af346b3bb
Stephen Watkins <stjowa@gmail.com>
2014-09-13 15:56:52 -0500

    Adding last activity update to migration.

--- db/migrations/009_remove_detail_constraint_from_datum.rb
+++ db/migrations/009_remove_detail_constraint_from_datum.rb
@@ -3,6 +3,12 @@ Sequel.migration do
     alter_table(:datums) do
       set_column_allow_null :detail
     end
+    Applyance::Server.db[:citizens].each do |citizen|
+      Applyance::Server.db[:citizens]
+        .where(:id => citizen[:id])
+        .where(:last_activity_at => nil)
+        .update(:last_activity_at => DateTime.now)
+    end
   end
   down do
     alter_table(:datums) do
