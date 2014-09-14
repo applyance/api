25306c43da97d6e9efcea06246ea374f69d2b7bc
Stephen Watkins <stjowa@gmail.com>
2014-09-14 15:13:21 -0500

    Fixing bug in migration.

--- db/migrations/010_remove_unique_slug_from_entities.rb
+++ db/migrations/010_remove_unique_slug_from_entities.rb
@@ -1,13 +1,14 @@
 Sequel.migration do
   up do
     alter_table(:entities) do
-      drop_constraint(:entities_slug_key)
+      drop_index [:slug], :unique => true, :name => :entities_slug_key
       add_index [:slug, :parent_id], :name => :entities_slug_parent_key, :unique => true
     end
   end
   down do
     alter_table(:entities) do
-      drop_constraint(:entities_slug_parent_key)
+      drop_index [:slug, :parent_id], :name => :entities_slug_parent_key, :unique => true
+      # drop_constraint(:entities_slug_parent_key)
       add_index [:slug], :unique => true, :name => :entities_slug_key
     end
   end
