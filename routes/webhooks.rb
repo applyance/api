99fe4fd6ad60a7f7ba9439b3e11ecfdc2fb52eed
Stephen Watkins <stjowa@gmail.com>
2014-09-17 11:59:14 -0500

    Updating stripe webhook for testing.

--- routes/webhooks.rb
+++ routes/webhooks.rb
@@ -5,6 +5,21 @@ module Applyance
       def self.registered(app)
 
         app.post '/webhooks/stripe', :provides => [:json] do
+          event_json = JSON.parse(request.body.read)
+
+          puts "Handling webhook from Stripe with type, [#{event_json["type"]}]"
+          puts "  ==  "
+          puts event_json.inspect
+          puts "  ==  "
+
+          case event_json["type"]
+          when "customer.subscription.updated"
+            puts "  Handling a subscription update from Stripe."
+
+          else
+            puts "  Unknown event type from Stripe."
+          end
+
           status 200
         end
 
